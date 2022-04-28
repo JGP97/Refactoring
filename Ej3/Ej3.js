d144ffc6deb95699feeeeff794c944a768d8cd51
LuisEladioSG <leladiosg@gmail.com>
2022-04-28 22:18:49 +0200

    Se han añadido cambios

--- Ej3/Ej3.js
+++ Ej3/Ej3.js
@@ -1,14 +1,7 @@
 function FunctionNumeros(){
-    var num;
-    for(i=0;i<=20;i++){
-        num=i;
-        if(i<10){
-            document.getElementById("1").innerHTML +=" "+ i;
-        }else{
-            if(num<=20){
-                document.getElementById("1").innerHTML+=" "+num;
-            }
-        }
-        
-    }
+
+    for(i=0; i<=20; i++)
+     {
+     document.getElementById("1").innerHTML +=" "+ i;
+     }       
 }
