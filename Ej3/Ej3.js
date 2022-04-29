38fd99ccadb96bff095dedaf05dce6910da85979
Santiago Herrera <santiagoherrerafernandez@fp.iesromerovargas.com>
2022-04-29 04:31:25 +0200

    Cambios en el ejercicio 3

--- Ej3/Ej3.js
+++ Ej3/Ej3.js
@@ -1,13 +1,12 @@
-function FunctionNumeros(){
-    var num;
+function EscribeNumeros1a20(){
+    let number;
     for(i=0;i<=20;i++){
-        num=i;
+        number=i;
         if(i<10){
             document.getElementById("1").innerHTML +=" "+ i;
-        }else{
-            if(num<=20){
-                document.getElementById("1").innerHTML+=" "+num;
-            }
+        }
+        else if(number<=20){
+            document.getElementById("1").innerHTML+=" "+number;
         }
         
     }
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Modificado ejercicio 2
 
 Los tipos de variables y sus nombres han sido actualizados.
 La id del parrafo solución del ejercicio ha sido actualizada.
+
+Modificado ejercicio 3
+Se ha cambiado el nombre de la función por uno más descriptivo de la función que cumple esta
+Se ha cambiado las variables var por let y cambiado el nombre
+Se ha reducido el tamaño del if
+
